fd2a246a2f823d9cac8693bbffa83f428cd984ed
singhotto <singhsahil3510@gmail.com>
2024-12-18 18:40:40 +0100

    Feat: increment ok

--- gif_maker/main.cpp
+++ gif_maker/main.cpp
@@ -3,8 +3,8 @@
 #include "utils/ImgGenerator.hh"
 
 int main(){
-    std::string init = "../data/input3.asp";
-    std::string res = "../data/data_3.txt";
+    std::string init = "../data/input1.asp";
+    std::string res = "../data/data_1.txt";
     ImgGenerator g(init, res);
     g.generate();
     return 0;
